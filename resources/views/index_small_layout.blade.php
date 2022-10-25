0e74d33aa38c96bb76382abe698acaada35315d2
hoanglong1407b <hoanglong1407b@gmail.com>
2022-10-26 01:27:29 +0700

    fix UI

--- resources/views/index_small_layout.blade.php
+++ resources/views/index_small_layout.blade.php
@@ -93,6 +93,7 @@
     
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script src="{{URL::asset('frontend/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('frontend/js/form.js')}}"></script>
     <script src="{{asset('frontend/js/checkInput.js')}}"></script>
 
     <!-- <script src="./frontend/css/bootstrap-4.2.1-dist/js/bootstrap.bundle.min.js"></script> -->
--- resources/views/indexfull_layout.blade.php
+++ resources/views/indexfull_layout.blade.php
@@ -45,7 +45,7 @@
                     </ul>
     
                     <ul class="navbar__list">
-                        <li class="navbar__item"><a href="paymentHistory.html" class="navbar__link">Kiểm tra đơn hàng</a></li>
+                        <li class="navbar__item"><a href="{{URL::to('/historyPayment')}}" class="navbar__link">Kiểm tra đơn hàng</a></li>
                         <li class="navbar__item"><a href="{{URL::to('/accountInfo')}}" class="navbar__link">HoangLong</a></li>
                         <li class="navbar__item"><a href="#" class="navbar__link">Đăng xuất</a></li>
                         <li class="navbar__item"><a href="{{URL::to('/signInSignUp')}}" class="navbar__link">Đăng nhập</a></li>
@@ -408,6 +408,7 @@
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script src="{{('frontend/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('frontend/js/view.js')}}"></script>
+    <script src="{{asset('frontend/js/form.js')}}"></script>
 
     <!-- <script src="./frontend/css/bootstrap-4.2.1-dist/js/bootstrap.bundle.min.js"></script> -->
    
