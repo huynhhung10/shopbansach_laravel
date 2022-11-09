0890ec28ad7fad9c6c5d503b0a31602da70201b8
huynhhung10 <100453652+huynhhung10@users.noreply.github.com>
2022-11-09 15:11:17 +0700

    them login register, customer  manage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,37 +42,37 @@ class DatabaseSeeder extends Seeder
         ]);
         // =================================== TẠO DỮ LIỆU CHO BRAND ============================================== 
 
-        DB::table('tbl_brand')->insert([
-            [
-                'product_name' => 'Starbuck',
-                'product_content' => 'Đây là content của Sách kinh tếshséhhhhhhhhhhhhhhhh quần áoĐây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áo',
-                'product_logo' => 'đây là logo cua sạđálkậbo',
-                'product_status' => 1,
-            ]
-        ]);
+        // DB::table('tbl_brand')->insert([
+        //     [
+        //         'product_name' => 'Starbuck',
+        //         'product_content' => 'Đây là content của Sách kinh tếshséhhhhhhhhhhhhhhhh quần áoĐây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áo',
+        //         'product_logo' => 'đây là logo cua sạđálkậbo',
+        //         'product_status' => 1,
+        //     ]
+        // ]);
 
-        // =================================== TẠO DỮ LIỆU CHO CATEGORY ==============================================
+        // // =================================== TẠO DỮ LIỆU CHO CATEGORY ==============================================
 
-        DB::table('tbl_category')->insert([
-            [
-                'product_name' => 'Sách kinh tế',
-                'product_status' => 1,
-            ]
-        ]);
+        // DB::table('tbl_category')->insert([
+        //     [
+        //         'product_name' => 'Sách kinh tế',
+        //         'product_status' => 1,
+        //     ]
+        // ]);
 
-        // =================================== TẠO DỮ LIỆU CHO PRODUCT ==============================================
+        // // =================================== TẠO DỮ LIỆU CHO PRODUCT ==============================================
 
-        DB::table('tbl_product')->insert([
-            [
-                'product_name' => 'Sách quản lý quần áo',
-                'product_content' => 'Đây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áo',
-                'product_price' => 200000,
-                'product_author' => 'Châu Nhựt Hưng',
-                'product_img' => 'Đây là đường link C:/alo/img',
-                'product_quantity' => 10,
-                'product_status' => 1,
-                'product_feature' => 1,
-            ]
-        ]);
+        // DB::table('tbl_product')->insert([
+        //     [
+        //         'product_name' => 'Sách quản lý quần áo',
+        //         'product_content' => 'Đây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áoĐây là content của Sách quản lý quần áo',
+        //         'product_price' => 200000,
+        //         'product_author' => 'Châu Nhựt Hưng',
+        //         'product_img' => 'Đây là đường link C:/alo/img',
+        //         'product_quantity' => 10,
+        //         'product_status' => 1,
+        //         'product_feature' => 1,
+        //     ]
+        // ]);
     }
 }
